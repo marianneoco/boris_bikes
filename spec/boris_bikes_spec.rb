88aa6a27871a551ac3e34ab6170df519702c1276
Marianne OConnor <marianneoco@gmail.com>
2019-10-29 14:53:08 +0000

    Set up spec file

---
+++ spec/boris_bikes_spec.rb
@@ -0,0 +1,12 @@
+#require
+
+describe DockingStation do
+
+end
+
+=begin
+it "makes an instance of the DockingStation class" do
+  docking_station = DockingStation.new
+  expect(docking_station).to be_instance_of(DockingStation)
+end
+=end
