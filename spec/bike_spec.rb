7e69a2941b2f4719c2b3e526e5b92bbefef46956
Marianne OConnor <marianneoco@gmail.com>
2019-10-31 17:41:32 +0000

    Failing test for user reporting bike as not working

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,8 +2,14 @@ require './lib/bike'
 
 describe Bike do
   it { should respond_to(:working?) }
-end
 
+  it "allows user to report bike as not working" do
+    bike = Bike.new
+    bike.working?(false)
+    expect(bike.working?).to eq false
+  end
+
+end
 =begin
 "responds to the method working?" do
   bike = Bike.new
